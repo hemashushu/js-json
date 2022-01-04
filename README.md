4c32368dd7b0148bae39a04bb394268d99c40fde
Hippo Spark (Arch) <hippospark@gmail.com>
2022-01-04 12:12:44 +0800

    添加了 README.md 文件.

---
+++ README.md
@@ -0,0 +1,3 @@
+# JS-Json
+
+超简陋的 JSON 字符串解析器，为没有 `JSON.parse()` 方法的 JS 环境提供 JSON 文本解析能力。
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2022 Hippospark <Hippospark@gmail.com>, All rights reserved.
+ * Copyright (c) 2022 Hemashushu <hippospark@gmail.com>, All rights reserved.
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this

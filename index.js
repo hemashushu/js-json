2507249d719ef8ae2f28f74d99d6d621c110b8ae
Hippo Spark (Arch) <hippospark@gmail.com>
2022-01-04 12:00:11 +0800

    首次提交.

---
+++ index.js
@@ -0,0 +1,15 @@
+/**
+ * Copyright (c) 2022 Hippospark <Hippospark@gmail.com>, All rights reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+import { JsonLex } from './src/json-lex.js';
+import { JsonParser } from './src/json-parser.js';
+
+export {
+    JsonLex,
+    JsonParser
+};
---
+++ test/test-all.js
@@ -0,0 +1,17 @@
+/**
+ * Copyright (c) 2022 Hippospark <Hippospark@gmail.com>, All rights reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+import { TestJsonLex } from "./test-json-lex.js";
+import { TestJsonParser } from "./test-json-parser.js";
+
+(() => {
+    TestJsonLex.testJsonLex();
+    TestJsonParser.testJsonParser();
+
+    console.log('All passed.')
+})();
